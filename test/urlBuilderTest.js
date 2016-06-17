c0a6f5acab68023e34e1382108653ce99b1dc704
Iain Johnston <Iain.Johnston@bbc.co.uk>
2016-06-17 23:10:29 +0100

    Add skelton of test and remove un-needed dep

---
+++ test/urlBuilderTest.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const assert = require('chai').assert;
+const sinon = require('sinon');
+
+sinon.assert.expose(assert, { prefix: '' });
+
+describe('URL Builder', function () {
+  describe('#createSiteRequestUrl', function () {
+    it('should return an array of archive urls', function () {
+        assert.equal(true, true);
+    });
+  });
+});
